faf75cd71770d189a21f135b8221a7f3add6db5b
unknown <Misha Conway@.(none)>
2012-02-07 12:54:37 -0800

    fix to get direct3d11 build compiling again

--- source/graphics/BaseEffect.h
+++ source/graphics/BaseEffect.h
@@ -7,6 +7,18 @@
 #include "../std/functional.h"
 #include "../interfaces/IValidatable.h"
 
+#ifdef _WIN32
+#define BEGIN_RENDER_TECHNIQUE(a, technique) a.RenderTechnique(technique,[this](){
+#define BEGIN_RENDER_CURRENT_TECHNIQUE(a) a.RenderTechnique(GetTechnique(),[this](){ 
+#endif
+
+#if defined(__APPLE__) || defined(__APPLE_CC__) 
+#define BEGIN_RENDER_TECHNIQUE(a, technique) a.RenderTechnique(technique,^(){
+#define BEGIN_RENDER_CURRENT_TECHNIQUE(a) a.RenderTechnique(GetTechnique(),^(){  
+#endif
+
+#define END_RENDER_TECHNIQUE });
+
 class BaseEffect : public IValidatable
 {
 public:
--- source/graphics/opengl/OpenGLEffect.h
+++ source/graphics/opengl/OpenGLEffect.h
@@ -9,18 +9,6 @@
 #include "../BaseEffect.h"
 #include <map>
 
-#ifdef _WIN32
-#define BEGIN_RENDER_TECHNIQUE(a, technique) a.RenderTechnique(technique,[this](){
-#define BEGIN_RENDER_CURRENT_TECHNIQUE(a) a.RenderTechnique(GetTechnique(),[this](){ 
-#endif
-
-#if defined(__APPLE__) || defined(__APPLE_CC__) 
-#define BEGIN_RENDER_TECHNIQUE(a, technique) a.RenderTechnique(technique,^(){
-#define BEGIN_RENDER_CURRENT_TECHNIQUE(a) a.RenderTechnique(GetTechnique(),^(){  
-#endif
-
-#define END_RENDER_TECHNIQUE });
-
 class OpenGLEffect : public BaseEffect
 {
 public:
