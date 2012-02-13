5d676ebcb3fa134a86e56f143d847bc6f0810b7f
Misha Conway <misha.conway@machinima.com>
2012-02-12 17:15:37 -0800

    added more info to opengl es capabilities

--- source/graphics/opengl/OpenGLCapabilities.cpp
+++ source/graphics/opengl/OpenGLCapabilities.cpp
@@ -16,6 +16,7 @@ OpenGLCapabilities::OpenGLCapabilities( OpenGLGraphicsDevice* open_gl_graphics_d
 #ifdef GL_ES_VERSION_2_0
 	supports_vertex_buffer_object = true; //vertex buffers are defined in core opengl ES spec
 	supports_pixel_buffer_object = false; //pixel buffer objects are not supported in opengl ES spec
+    supports_texture_array = false;
 	std::vector<std::string> extensions = ExplodeString( std::string( (char*) glGetString (GL_EXTENSIONS) ), " " );
 	//TODO: manually verify extensions here....
 #endif
